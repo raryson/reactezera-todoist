a3b2550df314ef1b1f0440f84e21ce9dfa5d7de3
Raryson Pereira Rost <raryson.rost@gmail.com>
2019-04-10 18:24:00 -0300

    Created delete on serverx

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
+let id = 0
+
 const app = express()
 
 app.use(bodyParser.json())
@@ -15,7 +17,11 @@ app.get('/users', (req, res) => {
 
 app.post('/users', (req, res) => {
     createUser(req.body.name, req.body.age)
-    console.log(database)
+    res.status(200).send()
+})
+
+app.delete('/users', (req, res) => {
+    deleteUser(req.body.id)
     res.status(200).send()
 })
 
@@ -23,6 +29,18 @@ app.listen(4000, () => {
     console.log('app running on 4000')
 })
 
+const deleteUser = (id) => {
+    const searchedUser = findUserById(id)
+    database.users.splice(searchedUser, 1)
+}
+
+const findUserById = (id) => {
+    database.users.filter((user) => {
+        return user.id === id
+    })
+}
+
 const createUser = (name, age) => {
-    database.users.push({name, age})
+    database.users.push({id, name, age})
+    this.id++
 }
